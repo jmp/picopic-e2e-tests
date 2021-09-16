8afb5b185a8ddd102b42ff4b1180beed984e1b6a
Jarkko Piiroinen <183207+jmp@users.noreply.github.com>
2021-09-16 19:30:54 +0300

    Use real website

--- tests/smoke.test.ts
+++ tests/smoke.test.ts
@@ -1,7 +1,7 @@
 import {test, expect} from '@playwright/test';
 
 test('smoke', async ({ page }) => {
-  await page.goto('http://localhost:3000');
+  await page.goto('https://picopic.io');
   const title = page.locator('h1');
   await expect(title).toHaveText('Picopic!');
 });
